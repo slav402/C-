d4a9d607af82b1bbcd4342a137261121179093b1
Slav402 <s.zekow@gmail.com>
2022-11-13 23:57:13 +0200

    First repo

---
+++ C#Basics/04.ConditionalStatements-Exercise/03.Time+15Minutes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Time_15minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes -= 60;
+            }
+
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            if (minutes >= 10)
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+        }
+    }
+}
