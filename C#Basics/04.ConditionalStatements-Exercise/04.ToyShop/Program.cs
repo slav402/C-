eee0a20d6f67e165719f44206586735edd6f903b
Slav402 <s.zekow@gmail.com>
2022-11-14 02:16:51 +0200

    added problem 04.ToyShop

---
+++ C#Basics/04.ConditionalStatements-Exercise/04.ToyShop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double holidayPrice = double.Parse(Console.ReadLine());
+            List<double> priceList = new List<double> 
+            { 
+                2.60, 
+                3.0, 
+                4.10, 
+                8.20, 
+                2.0 
+            };
+
+            int countOfToys = 0;
+            double money = 0;
+
+            for (int i = 0; i <= 4; i++)
+            {
+                int count = int.Parse(Console.ReadLine());
+                double price = count * priceList[i];
+
+                money += price;
+                countOfToys += count;
+            }
+
+            if (countOfToys >= 50)
+            {
+                money *= 0.75;
+            }
+
+            money *= 0.9;
+
+            if (money >= holidayPrice)
+            {
+                Console.WriteLine($"Yes! {(money - holidayPrice):F2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {(holidayPrice - money):F2} lv needed.");
+            }
+        }
+    }
+}
--- C#Basics/04.ConditionalStatements-Exercise/06.WorldSwimmingRecord/Program.cs
+++ C#Basics/04.ConditionalStatements-Exercise/06.WorldSwimmingRecord/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace _06.WorldSwimmingRecord
+namespace _07.WorlSwimingRecord
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double currentREC = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double secondsPerMeter = double.Parse(Console.ReadLine());
+
+            double swimmingTime = distance * secondsPerMeter;
+            double delay = Math.Floor(distance / 15) * 12.5;
+
+            double result = swimmingTime + delay;
+
+            if (result < currentREC)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {result:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {(result - currentREC):F2} seconds slower.");
+            }
         }
     }
 }
