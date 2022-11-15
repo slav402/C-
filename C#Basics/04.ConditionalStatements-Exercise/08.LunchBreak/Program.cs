28c4fa784e32992be538375185a1c94b53518710
Slav402 <s.zekow@gmail.com>
2022-11-16 01:21:59 +0200

    adding problem 08.LunchBreak

---
+++ C#Basics/04.ConditionalStatements-Exercise/08.LunchBreak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08.LunchBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            double movieLenght = double.Parse(Console.ReadLine());
+            double breack = double.Parse(Console.ReadLine());
+
+            double lunchTime = breack * 1 / 8;
+            double restTime = breack * 1 / 4;
+
+            double freeTime = breack - lunchTime - restTime;
+
+            if (freeTime >= movieLenght)
+            {
+                Console.WriteLine($"You have enough time to watch {movie} and left with {Math.Ceiling(freeTime - movieLenght)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {movie}, you need {Math.Ceiling(movieLenght-freeTime)} more minutes.");
+            }
+        }
+    }
+}
