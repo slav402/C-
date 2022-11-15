a0e0e08f6c915ba0e7853661397f31e43413e7fe
Slav402 <s.zekow@gmail.com>
2022-11-15 23:57:59 +0200

    Adding problem 07.Shopping

---
+++ C#Basics/04.ConditionalStatements-Exercise/07.Shopping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07.Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int numberVideoCards = int.Parse(Console.ReadLine());
+            int numberCPU = int.Parse(Console.ReadLine());
+            int numberRAM = int.Parse(Console.ReadLine());
+
+            double vCardsPrice = numberVideoCards * 250;
+
+            double sumCPU = vCardsPrice * 0.35 * numberCPU;
+
+            double sumRAM = vCardsPrice * 0.1 * numberRAM;
+
+            double bill = vCardsPrice + sumCPU + sumRAM;
+
+            if (bill <= budget)
+            {
+                Console.WriteLine($"You have {budget - bill} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {bill - budget} leva more!");
+            }
+
+        }
+    }
+}
