d775dd90fb381c57bfd373067502eacc9c5d9e6a
Slav402 <s.zekow@gmail.com>
2022-11-18 22:36:42 +0200

    Adding more problem solvings

---
+++ C#Basics/05.ConditionalStatementsAdvanced-Lab/04.PersonalTitles/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04.PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (age < 16)
+            {
+                if (gender == "m")
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+            else
+            {
+                if (gender == "m")
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+        }
+    }
+}
