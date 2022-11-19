4655ee169bf5373eda2b4e6b007978353feda719
Slav402 <s.zekow@gmail.com>
2022-11-19 22:36:01 +0200

    adding problem 8.CinemaTicket

---
+++ C#Basics/05.ConditionalStatementsAdvanced-Lab/08.CinemaTicket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.CinemaTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+
+            switch (day.ToLower())
+            {
+                case "monday":
+                case "tuesday":
+                case "friday":
+                    Console.WriteLine(12);
+                    break;
+
+                case "wednesday":
+                case "thursday":
+                    Console.WriteLine(14);
+                    break;
+
+                default:
+                    Console.WriteLine(16);
+                    break;
+            }
+        }
+    }
+}
