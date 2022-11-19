1c38d6deead953051fa80c351931668d597f9bcc
Slav402 <s.zekow@gmail.com>
2022-11-19 23:36:18 +0200

    some update

---
+++ C#Basics/05.ConditionalStatementsAdvanced-Lab/09.FruitOrVegetable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.FruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] fruit = new string[] { "banana", "apple", "kiwi", "cherry", "lemon", "grapes" };
+
+            int someNum = Array.IndexOf(fruit, input);
+
+            if (someNum > -1)
+            {
+                Console.WriteLine("fruit");
+            }
+            else
+            {
+                Console.WriteLine("vegetable");
+            }
+        }
+    }
+}
