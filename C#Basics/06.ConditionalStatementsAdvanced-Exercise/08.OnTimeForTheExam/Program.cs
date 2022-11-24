e52ac17beae39c5815d4397429929ee48304407b
Slav402 <s.zekow@gmail.com>
2022-11-24 22:31:52 +0200

    addin problem 08.OnTimeForTheExam

---
+++ C#Basics/06.ConditionalStatementsAdvanced-Exercise/08.OnTimeForTheExam/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _08.OnTimeForTheExam
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int examHour = int.Parse(Console.ReadLine());
+			int examMinutes = int.Parse(Console.ReadLine());
+			int entryHour = int.Parse(Console.ReadLine());
+			int entryMinutes = int.Parse(Console.ReadLine());
+
+			if (entryHour == examHour)
+			{
+				if (entryMinutes <= examMinutes)
+				{
+
+					if ((examMinutes - entryMinutes) <= 30)
+					{
+						Console.WriteLine("on time");
+						Console.WriteLine("{0} minutes before the start", examMinutes - entryMinutes);
+					}
+					else if ((examMinutes - entryMinutes) > 30)
+					{
+						Console.WriteLine("early");
+						Console.WriteLine("{0} minutes before the start", examMinutes - entryMinutes);
+					}
+				}
+				else
+				{
+					Console.WriteLine("Late");
+					Console.WriteLine("{0} minutes after the start", entryMinutes - examMinutes);
+				}
+
+			}
+			else if (entryHour < examHour)
+			{
+				int delay = ((examHour * 60) + examMinutes) - ((entryHour * 60) + entryMinutes);
+
+				int hours = delay / 60;
+				int minutes = delay % 60;
+
+				if (delay <= 30)
+				{
+					Console.WriteLine("on time");
+					Console.WriteLine("{0} minutes before the start", (examMinutes - entryMinutes + 60));
+				}
+				else if (delay < 60)
+				{
+					Console.WriteLine("Early");
+					Console.WriteLine("{0} minutes before the start", delay);
+				}
+				else
+				{
+					Console.WriteLine("Early");
+					Console.WriteLine("{0}:{1:D2} hours before the start", hours, minutes);
+				}
+			}
+			else
+			{
+				int delay = ((entryHour * 60) + entryMinutes) - ((examHour * 60) + examMinutes);
+
+				int hours = delay / 60;
+				int minutes = delay % 60;
+
+				if (delay < 60)
+				{
+					Console.WriteLine("Late");
+					Console.WriteLine("{0} minutes after the start", delay);
+				}
+				else
+				{
+					Console.WriteLine("Late");
+					Console.WriteLine("{0}:{1:D2} hours after the start", hours, minutes);
+				}
+			}
+		}
+	}
+}
