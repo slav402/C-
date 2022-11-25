10e815321cbb9c5e17677a8ae831399ef4153274
Slav402 <s.zekow@gmail.com>
2022-11-25 23:34:36 +0200

    adding new theme For Loops and problems from 1 to 5

---
+++ C#Basics/07.ForLoop-Lab/05.CharacterSequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05.CharacterSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                char leter = word[i];
+                Console.WriteLine(leter);
+            }
+        }
+    }
+}
