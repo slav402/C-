bd95a07f1802f0c55e59b230fbf33d67d9e51069
Slav402 <s.zekow@gmail.com>
2022-12-15 23:41:27 +0200

    adding new theme with few ssolutions

---
+++ C#Basics/08.ForLoop-Exercise/08.ForLoop-Exercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxValue = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentInt = int.Parse(Console.ReadLine());
+                sum += currentInt;
+
+                if (currentInt > maxValue)
+                {
+                    maxValue = currentInt;
+                }
+            }
+
+            int sumWitoutMaxNum = sum - maxValue;
+
+            if (sumWitoutMaxNum == maxValue)
+            {
+                Console.WriteLine($"Yes\nSum = {maxValue}");
+            }
+            else
+            {
+                Console.WriteLine($"No\nDiff = {Math.Abs(sumWitoutMaxNum - maxValue)}");
+            }
+        }
+    }
+}
---
+++ C#Basics/08.ForLoop-Exercise/08.ForLoop-Exercise/03.Histogram/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace E04.Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            double p4 = 0;
+            double p5 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 200)
+                {
+                    p1++;
+                }
+                else if (number <= 399)
+                {
+                    p2++;
+                }
+                else if (number <= 599)
+                {
+                    p3++;
+                }
+                else if (number <= 799)
+                {
+                    p4++;
+                }
+                else
+                {
+                    p5++;
+                }
+            }
+
+            Console.WriteLine("{0:F2}%", (p1 / n * 100));
+            Console.WriteLine("{0:F2}%", (p2 / n * 100));
+            Console.WriteLine("{0:F2}%", (p3 / n * 100));
+            Console.WriteLine("{0:F2}%", (p4 / n * 100));
+            Console.WriteLine("{0:F2}%", (p5 / n * 100));
+        }
+    }
+}
