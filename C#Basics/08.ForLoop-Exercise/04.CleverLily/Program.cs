6a45b0fc07bd7ba609022dead85d467a46b21a4e
Slav402 <s.zekow@gmail.com>
2022-12-21 23:49:57 +0200

    add problem 08.CleverLili

---
+++ C#Basics/08.ForLoop-Exercise/04.CleverLily/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _04.CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liliAge = int.Parse(Console.ReadLine());
+            double priceOfWashingMashine = double.Parse(Console.ReadLine());
+            double toyPrice = double.Parse(Console.ReadLine());
+
+            int toyCount = 0;
+            double money = 0;
+            int counter = 0;
+
+            for (int i = 1; i <= liliAge; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    counter++;
+                    money += (10 * counter - 1);
+                }
+                else
+                {
+                    //toyCount++;
+                    money += toyPrice;
+                }
+            }
+
+            if (money < priceOfWashingMashine)
+            {
+                Console.WriteLine($"No! {priceOfWashingMashine - money}");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! {(money - priceOfWashingMashine):f2}");
+            }
+        }
+    }
+}
