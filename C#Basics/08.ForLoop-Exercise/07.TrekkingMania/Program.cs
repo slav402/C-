640da1003d3a3d4c099ad17cc1d413c08b1e7e4b
Slav402 <s.zekow@gmail.com>
2022-12-25 16:41:03 +0200

    adding problem 07.Trekking Mania

--- C#Basics/08.ForLoop-Exercise/07.TrekkingMania/Program.cs
+++ C#Basics/08.ForLoop-Exercise/07.TrekkingMania/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07.TrekkingMania
 {
@@ -6,7 +8,63 @@ namespace _07.TrekkingMania
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int countOfGroups = int.Parse(Console.ReadLine());
+            int totalKlimbers = 0;
+            int everest = 0;
+            int k2 = 0;
+            int kilimanjaro = 0;
+            int monthblan = 0;
+            int musala = 0;
+
+            //int[] peaks = new int[] {musala, k2, kilimanjaro, monthblan, musala};
+
+            for (int i = 0; i < countOfGroups; i++)
+            {
+                int dudes = int.Parse(Console.ReadLine());
+                totalKlimbers += dudes;
+
+                if (dudes >= 41)
+                {
+                    everest += dudes;
+                }
+                else if (dudes >= 26)
+                {
+                    k2 += dudes;
+                }
+                else if (dudes >= 13)
+                {
+                    kilimanjaro += dudes;
+                }
+                else if (dudes >= 6)
+                {
+                    monthblan += dudes;
+                }
+                else
+                {
+                    musala += dudes;
+                }
+            }
+
+
+            GetPercent(musala, totalKlimbers);
+            GetPercent(monthblan, totalKlimbers);
+            GetPercent(kilimanjaro, totalKlimbers);
+            GetPercent(k2, totalKlimbers);
+            GetPercent(everest, totalKlimbers);
+            
+
+            //Console.WriteLine($"{((musala/totalKlimbers)*100):f2}%");
+            //Console.WriteLine($"{((monthblan/totalKlimbers)*100):f2}%");
+            //Console.WriteLine($"{((kilimanjaro/totalKlimbers)*100):f2}%");
+            //Console.WriteLine($"{((k2/totalKlimbers)*100):f2}%");
+            //Console.WriteLine($"{((everest/totalKlimbers)*100):f2}%");
+
+
+        }
+
+        private static void GetPercent(int musala, int totalKlimbers)
+        {
+            Console.WriteLine($"{((double)musala / (double)totalKlimbers * 100):f2}%");
         }
     }
 }
