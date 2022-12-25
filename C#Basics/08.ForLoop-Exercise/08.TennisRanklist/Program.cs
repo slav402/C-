05931799331ccf9e4f5c53160ce576936edeec45
Slav402 <s.zekow@gmail.com>
2022-12-25 17:16:25 +0200

    upload problem 08.Tennis Ranklist

---
+++ C#Basics/08.ForLoop-Exercise/08.TennisRanklist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08.TennisRanklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int points = int.Parse(Console.ReadLine());
+            int startPoints = points;
+            int countsOfWins = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string tournamentResult = Console.ReadLine();
+
+                switch (tournamentResult)
+                {
+                    case "W": points += 2000;
+                              countsOfWins++;
+                        break;
+
+                    case "F": points += 1200; 
+                        break;
+
+                    case "SF": points += 720; 
+                        break;
+                    
+                    default: break;
+                }
+            }
+
+            Console.WriteLine($"Final points: {points}");
+            Console.WriteLine($"Average points: {(points - startPoints)/n}");
+            Console.WriteLine($"{(double)countsOfWins / (double)n}%");
+        }
+    }
+}
