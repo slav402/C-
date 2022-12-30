009f322dcb28eebf4f2412daa8c7b0a672970517
Slav402 <s.zekow@gmail.com>
2022-12-30 23:45:37 +0200

    uploading task 05.Coins

---
+++ C#Basics/10.WhileLoop-Exercise/05.Coins/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _05._Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double change = double.Parse(Console.ReadLine());
+            double changeToCoins = Math.Floor(change * 100);
+            int counter = 0;
+
+            while (changeToCoins != 0)
+            {
+                if (changeToCoins >= 200)
+                {
+                    changeToCoins -= 200;
+                    counter++;
+                }
+                else if (changeToCoins >= 100)
+                {
+                    changeToCoins -= 100;
+                    counter++;
+                }
+                else if (changeToCoins >= 50)
+                {
+                    changeToCoins -= 50;
+                    counter++;
+                }
+                else if (changeToCoins >= 20)
+                {
+                    changeToCoins -= 20;
+                    counter++;
+                }
+                else if (changeToCoins >= 10)
+                {
+                    changeToCoins -= 10;
+                    counter++;
+                }
+                else if (changeToCoins >= 5)
+                {
+                    changeToCoins -= 5;
+                    counter++;
+                }
+                else if (changeToCoins >= 2)
+                {
+                    changeToCoins -= 2;
+                    counter++;
+                }
+                else if (changeToCoins >= 1)
+                {
+                    changeToCoins -= 1;
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
