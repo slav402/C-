0d2adadf858435f21c0a3490574fb352d3162728
Slav402 <s.zekow@gmail.com>
2022-12-31 18:00:41 +0200

    adding 04.Walking and 06.Cake

---
+++ C#Basics/10.WhileLoop-Exercise/04.Walking/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _04.Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 0;
+            int targetSteps = 10000;
+
+            while (steps < targetSteps)
+            {
+                string enter = Console.ReadLine();
+
+                if (enter == "Going home")
+                {
+                    steps += int.Parse(Console.ReadLine());
+                    break;
+                }
+
+                steps += int.Parse(enter);
+            }
+
+            if (steps >= targetSteps)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{steps - targetSteps} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{targetSteps - steps} more steps to reach goal.");
+            }
+        }
+    }
+}
---
+++ C#Basics/10.WhileLoop-Exercise/06.Cake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            int countOfPieces = lenght * width;
+            
+            string input = Console.ReadLine();
+
+            while (input != "STOP")
+            {
+                countOfPieces -= int.Parse(input);
+
+                if (countOfPieces <= 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(countOfPieces)} pieces more.");
+                    return;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{countOfPieces} pieces are left.");
+        }
+    }
+}
