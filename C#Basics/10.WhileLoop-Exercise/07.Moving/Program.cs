3cb0cb25bf85f2874e81ce1ededb1f86cfbd4484
Slav402 <s.zekow@gmail.com>
2023-01-01 17:07:27 +0200

    uploading the last task 07.Moving

--- C#Basics/10.WhileLoop-Exercise/07.Moving/Program.cs
+++ C#Basics/10.WhileLoop-Exercise/07.Moving/Program.cs
@@ -6,7 +6,26 @@ namespace _07.Moving
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int heigh = int.Parse(Console.ReadLine());
+
+            int freeRoom = width * lenght * heigh;
+
+            while (freeRoom > 0)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Done")
+                {
+                    Console.WriteLine($"{freeRoom} Cubic meters left.");
+                    return;
+                }
+
+                freeRoom -= int.Parse(input);
+            }
+
+            Console.WriteLine($"No more free space! You need {Math.Abs(freeRoom)} Cubic meters more.");
         }
     }
 }
