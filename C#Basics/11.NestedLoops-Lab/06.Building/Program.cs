4ee6c0ad402fd92b0e52fedb6abf4f1e9e1b26a2
Slav402 <s.zekow@gmail.com>
2023-01-04 23:34:39 +0200

    Adding last task 06.Buildng

---
+++ C#Basics/11.NestedLoops-Lab/06.Building/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rooms; i++)
+            {
+                Console.Write($"L{levels}{i} ");
+            }
+            Console.WriteLine();
+
+            if (levels > 1)
+            {
+                for (int i = levels-1; i > 0; i--)
+                {
+                    for (int j = 0; j < rooms; j++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.Write($"O{i}{j} ");
+                        }
+                        else
+                        {
+                            Console.Write($"A{i}{j} ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
