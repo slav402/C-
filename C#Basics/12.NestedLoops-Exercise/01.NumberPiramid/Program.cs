50dc456a99e8e6774f2308452d07aa960b86d6ce
Slav402 <s.zekow@gmail.com>
2023-01-05 23:19:37 +0200

    the first task

---
+++ C#Basics/12.NestedLoops-Exercise/01.NumberPiramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int inputNumber = int.Parse(Console.ReadLine());
+		int counter = 0;
+
+		for (int i = 1; i <= inputNumber; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				counter++;
+
+				if (counter <= inputNumber)
+				{
+					Console.Write(counter + " ");
+				}
+				else
+				{
+					break;
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+}
