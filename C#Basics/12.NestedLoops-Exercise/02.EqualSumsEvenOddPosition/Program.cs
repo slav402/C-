e012c6cae7fafde8d1bb0a593c6abbeb979cbe47
Slav402 <s.zekow@gmail.com>
2023-01-06 23:35:16 +0200

    adding second task

---
+++ C#Basics/12.NestedLoops-Exercise/02.EqualSumsEvenOddPosition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int firstNum = int.Parse(Console.ReadLine());
+		int secondNum = int.Parse(Console.ReadLine());
+
+		for (int i = firstNum; i <= secondNum; i++)
+		{
+			int evenSum = 0;
+			int oddSum = 0;
+			string numToString = i.ToString();
+
+			for (int j = 0; j < numToString.Length; j++)
+			{
+				int currentPosition = int.Parse(numToString[j].ToString());
+
+				if (j % 2 == 0)
+				{
+					evenSum += currentPosition;
+				}
+				else
+				{
+					oddSum += currentPosition;
+				}
+			}
+
+			if (evenSum == oddSum)
+			{
+				Console.Write(i + " ");
+			}
+		}
+	}
+}
