f59c0dcc7c2da66414fd60d84ea38bd7e9a5fc81
Slav402 <s.zekow@gmail.com>
2023-01-10 23:48:45 +0200

    adding first task for Fundamentals level Convert Meters To Kilometers

---
+++ C#Fundamentals/01.DataTypesAndVariables-Lab/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.ConvertMetersToKilometers
+{
+    class ConvertMetersToKilometers
+    {
+        static void Main(string[] args)
+        {
+            decimal meters = decimal.Parse(Console.ReadLine());
+            decimal kilometers = meters / 1000.0M;
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
