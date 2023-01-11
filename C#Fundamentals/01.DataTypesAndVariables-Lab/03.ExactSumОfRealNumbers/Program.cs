1837132df6cfa3f818406de237ec089415c69188
Slav402 <s.zekow@gmail.com>
2023-01-11 23:27:49 +0200

    Add two tasks 04 i 05

---
+++ C#Fundamentals/01.DataTypesAndVariables-Lab/03.ExactSumОfRealNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    class ExactSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            int countOfNUmbers = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 1; i <= countOfNUmbers; i++)
+            {
+                sum += decimal.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
