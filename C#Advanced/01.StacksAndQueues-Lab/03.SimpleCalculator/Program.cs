11cc9477ac6fe28745fd21a2a6f536b17d5d590e
Slav402 <s.zekow@gmail.com>
2023-01-13 00:22:30 +0200

    Upload task 03.SipleCalculator

---
+++ C#Advanced/01.StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> expresion = new Stack<string>(input.Reverse());
+
+            while (expresion.Count > 1)
+            {
+                int firstOperand = int.Parse(expresion.Pop());
+                string action = expresion.Pop();
+                int secondOperand = int.Parse(expresion.Pop());
+
+                if (action == "+")
+                {
+                    expresion.Push((firstOperand + secondOperand).ToString());
+                }
+                else
+                {
+                    expresion.Push((firstOperand - secondOperand).ToString());
+                }
+            }
+
+            Console.WriteLine(expresion.Pop());
+        }
+    }
+}
