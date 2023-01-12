241acb22b586e89f8ac0855a572f869e4e74d793
Slav402 <s.zekow@gmail.com>
2023-01-12 23:45:33 +0200

    Add first two task 01 and 02

---
+++ C#Advanced/01.StacksAndQueues-Lab/01.ReverseOfString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reversed = new Stack<char>();
+
+            foreach (var ch in input)
+            {
+                reversed.Push(ch);
+            }
+
+            while(reversed.Count != 0)
+            {
+                Console.Write(reversed.Pop());
+            }
+        }
+    }
+}
---
+++ C#Advanced/01.StacksAndQueues-Lab/02.StackSum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Stack<int> expresion = new Stack<int>(input);
+
+            List<string> comand = Console.ReadLine().Split().ToList();
+
+            while (comand[0].ToLower() != "end")
+            {
+                if (comand[0].ToLower() == "add")
+                {
+                    expresion.Push(int.Parse(comand[1]));
+                    expresion.Push(int.Parse(comand[2]));
+                }
+                else
+                {
+                    int removeCount = int.Parse(comand[1]);
+
+                    if (expresion.Count > removeCount)
+                    {
+                        for (int i = 0; i < removeCount; i++)
+                        {
+                            expresion.Pop();
+                        }
+                    }
+                }
+
+                comand = Console.ReadLine().Split().ToList();
+            }
+
+            Console.WriteLine($"Sum: {expresion.Sum()}");
+        }
+    }
+}
