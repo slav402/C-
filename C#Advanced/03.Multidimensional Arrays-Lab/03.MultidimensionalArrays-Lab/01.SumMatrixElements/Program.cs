eec37d16ccdd31a1ebf04288209293cb572a8c21
Slav402 <s.zekow@gmail.com>
2023-01-16 23:04:27 +0200

    adding first task

---
+++ C#Advanced/03.Multidimensional Arrays-Lab/03.MultidimensionalArrays-Lab/01.SumMatrixElements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		int[] rangeInput = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+		int rows = rangeInput[0];
+		int columns = rangeInput[1];
+		int matrixSum = 0;
+
+		for (int i = 0; i < rows; i++)
+		{
+			int[] inputMembers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+			matrixSum += inputMembers.Sum();
+		}
+
+		Console.WriteLine("{0}\n{1}\n{2}", rows, columns, matrixSum);
+	}
+}
