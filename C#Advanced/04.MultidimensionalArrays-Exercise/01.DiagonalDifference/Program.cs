f66fd4261649be79dd1abc62466ad912b7906cca
Slav402 <s.zekow@gmail.com>
2023-01-20 23:05:05 +0200

    Add tasks 01 and 02 from theme MultidimensionalArrays-Exercise

---
+++ C#Advanced/04.MultidimensionalArrays-Exercise/01.DiagonalDifference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int sumMainDiag = 0;
+            int sumSecDiag = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = input[j];
+
+                    if (i == j)
+                    {
+                        sumMainDiag += input[j];
+                        sumSecDiag += input[n - j - 1];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumMainDiag - sumSecDiag));
+        }
+    }
+}
---
+++ C#Advanced/04.MultidimensionalArrays-Exercise/02.2X2SquaresInMatrix/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._2X2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = size[0];
+            int m = size[1];
+            char[,] matrix = new char[n,m];
+            int counter = 0;
+
+            GetMatrix(matrix);
+
+
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = 0; j < m-1; j++)
+                {
+                    if (matrix[i,j] == matrix[i, j+1] &&
+                        matrix[i,j] == matrix[i+1, j+1] &&
+                        matrix[i,j] == matrix[i+1, j])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+
+        private static void GetMatrix(char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                
+                char[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = input[j];
+                }
+            }
+        }
+    }
+}
