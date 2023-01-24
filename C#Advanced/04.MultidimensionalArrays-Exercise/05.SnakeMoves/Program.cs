8a3e39a0f1d0261313afec24cecf7a3e286f927f
Slav402 <s.zekow@gmail.com>
2023-01-24 23:04:55 +0200

    add task 06.JaggedArrayManilupator

---
+++ C#Advanced/04.MultidimensionalArrays-Exercise/05.SnakeMoves/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = size[0];
+            int cols = size[1];
+            string[,] matrix = new string[rows, cols];
+            int counter = 0;
+
+            string theSnake = Console.ReadLine();
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        matrix[i, j] = theSnake[counter % theSnake.Length].ToString();
+                        counter++;
+                    }    
+                }
+                else
+                {
+                    for (int j = cols-1; j >= 0; j--)
+                    {
+                        matrix[i, j] = theSnake[counter % theSnake.Length].ToString();
+                        counter++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#Advanced/04.MultidimensionalArrays-Exercise/06.JaggedArrayManipulator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.JaggedArrayManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] jaggedMatrix = new int[n][];
+
+            for (int i = 0; i < jaggedMatrix.Length; i++)
+            {
+                int[] currentLine = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                jaggedMatrix[i] = currentLine;
+            }
+
+            for (int i = 0; i < jaggedMatrix.Length-1; i++)
+            {
+                if (jaggedMatrix[i].Length == jaggedMatrix[i+1].Length)
+                {
+                    for (int j = 0; j < jaggedMatrix[i].Length; j++)
+                    {
+                        jaggedMatrix[i][j] *= 2;
+                        jaggedMatrix[i+1][j] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < jaggedMatrix[i].Length; j++)
+                    {
+                        jaggedMatrix[i][j] /= 2;
+                    }
+
+                    for (int j = 0; j < jaggedMatrix[i+1].Length; j++)
+                    {
+                        jaggedMatrix[i+1][j] /= 2;
+                    }
+                }
+            }
+
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            while (input[0].ToLower() != "end")
+            {
+                string command = input[0];
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+                int value = int.Parse(input[3]);
+
+                if (row >= 0 && row < jaggedMatrix.Length && col >= 0 && col < jaggedMatrix[row].Length)
+                {
+                    if (command == "Add" )
+                    {
+                        jaggedMatrix[row][col] += value;
+                    }
+                    else
+                    {
+                        jaggedMatrix[row][col] -= value;
+                    }
+                }
+
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            }
+
+            for (int i = 0; i < jaggedMatrix.Length; i++)
+            {
+                for (int j = 0; j < jaggedMatrix[i].Length; j++)
+                {
+                    Console.Write($"{jaggedMatrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
