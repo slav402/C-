f355db02aa6006c06ff12b4d0f21c6990c5e7496
Slav402 <s.zekow@gmail.com>
2023-02-01 00:40:28 +0200

    Loading new Chapter SetsAndDictionarioes-Exercise and task 01. 02. and 03.

---
+++ C#Advanced/05.SetsAndDictionaries-Lab/06.SetsAndDictionaries-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int turns = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < turns; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(na);
+            }
+        }
+    }
+}
---
+++ C#Advanced/05.SetsAndDictionaries-Lab/06.SetsAndDictionaries-Exercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = range[0];
+            int m = range[1];
+
+            HashSet<int> setN = new HashSet<int>();
+            HashSet<int> setM = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                setN.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                setM.Add(int.Parse(Console.ReadLine()));
+            }
+
+            //setN.UnionWith(setM);
+            //setn.exceptwith(setm);
+            //setN.SymmetricExceptWith(setM);
+
+            setN.IntersectWith(setM);
+
+            Console.WriteLine(String.Join(" ", setN));
+        }
+    }
+}
