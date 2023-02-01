1d92b620f26b128b24aa99fdc247da521379ce1b
Slav402 <s.zekow@gmail.com>
2023-02-02 00:14:56 +0200

    adding task 10.ForceBook

---
+++ C#Advanced/05.SetsAndDictionaries-Lab/06.SetsAndDictionaries-Exercise/04.EvenTimes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int curentNum = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(curentNum))
+                {
+                    numbers.Add(curentNum, 0);
+                }
+
+                numbers[curentNum]++;
+            }
+
+            Console.WriteLine(numbers.Single(x => x.Value % 2 == 0).Key);
+        }
+    }
+}
---
+++ C#Advanced/05.SetsAndDictionaries-Lab/06.SetsAndDictionaries-Exercise/10.FprceBook/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.FprceBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, SortedSet<string>> userSide = new SortedDictionary<string, SortedSet<string>>();
+
+            string entry = string.Empty;
+
+            while ((entry = Console.ReadLine()) != "Lumpawaroo")
+            {
+                if (entry.Contains("|"))
+                {
+                    string[] tokens = entry.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string side = tokens[0];
+                    string user = tokens[1];
+
+                    if (!userSide.Values.Any(n => n.Contains(user)))
+                    {
+                        if (!userSide.ContainsKey(side))
+                        {
+                            userSide.Add(side, new SortedSet<string>());
+                        }
+                        userSide[side].Add(user);
+                    }
+                }
+                else
+                {
+                    string[] tokens = entry.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string user = tokens[0];
+                    string side = tokens[1];
+
+                    foreach (var sides in userSide)
+                    {
+                        if (sides.Value.Contains(user))
+                        {
+                            sides.Value.Remove(user);
+                        }
+                    }
+                    
+                    if (!userSide.ContainsKey(side))
+                    {
+                        userSide.Add(side, new SortedSet<string>());
+                    }
+
+                    userSide[side].Add(user);
+                    Console.WriteLine($"{user} joins the {side} side!");
+                }
+            }
+
+            //Dictionary<string, SortedSet<string>> result = userSide.OrderByDescending(u => u.Value.Count).ToDictionary(s => s.Key, u => u.Value);
+            var result = userSide.OrderByDescending(u => u.Value.Count);
+
+            foreach (var sides in result)
+            {
+                if (sides.Value.Count > 0)
+                {
+                    Console.WriteLine($"Side: {sides.Key}, Members: {sides.Value.Count}");
+
+                    foreach (var item in sides.Value)
+                    {
+                        Console.WriteLine($"! {item}");
+                    }
+                }
+            }
+        }
+    }
+}
