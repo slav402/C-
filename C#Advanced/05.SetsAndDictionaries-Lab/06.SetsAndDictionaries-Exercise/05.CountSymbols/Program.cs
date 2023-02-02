cd5b292e66fe820acdb8124f8c03d0f1850016c1
Slav402 <s.zekow@gmail.com>
2023-02-03 00:44:33 +0200

    upload tasks 05. and 06. from chapter SetsAndDictionaries

---
+++ C#Advanced/05.SetsAndDictionaries-Lab/06.SetsAndDictionaries-Exercise/05.CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Dictionary<char, int> symbols = new Dictionary<char, int>();
+
+			string input = Console.ReadLine();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (!symbols.ContainsKey(input[i]))
+				{
+					symbols.Add(input[i], 0);
+				}
+
+				symbols[input[i]]++;
+			}
+
+			symbols = symbols.OrderBy(x => x.Key).ToDictionary(y => y.Key, z => z.Value);
+
+			foreach (var item in symbols)
+			{
+				Console.WriteLine("{0}: {1} time/s", item.Key, item.Value);
+			}
+		}
+    }
+}
---
+++ C#Advanced/05.SetsAndDictionaries-Lab/06.SetsAndDictionaries-Exercise/06.Wardrobe/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] entry = Console.ReadLine()
+                    .Split(new string[] { " -> ", "," }, StringSplitOptions.RemoveEmptyEntries);
+
+                string color = entry[0];
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                for (int j = 1; j < entry.Length; j++)
+                {
+                    if (!wardrobe[color].ContainsKey(entry[j]))
+                    {
+                        wardrobe[color].Add(entry[j], 0);
+                    }
+
+                    wardrobe[color][entry[j]]++;
+                }
+            }
+
+            string[] searchComand = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string neededColor = searchComand[0];
+            string neededClothe = searchComand[1];
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var cloath in color.Value)
+                {
+                    Console.Write($"* {cloath.Key} - {cloath.Value}");
+
+                    if (color.Key == neededColor && cloath.Key == neededClothe)
+                    {
+                        Console.Write(" (found!)");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
