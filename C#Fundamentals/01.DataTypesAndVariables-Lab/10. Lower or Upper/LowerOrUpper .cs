b80f26bd01f4d88a96eafe075126fbf6aa21c07d
Slav402 <s.zekow@gmail.com>
2023-02-10 21:40:47 +0200

    upload task 10.LowerOrUpper from DataTypesAndVariables-Lab

---
+++ C#Fundamentals/01.DataTypesAndVariables-Lab/10. Lower or Upper/LowerOrUpper .cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char leter = char.Parse(Console.ReadLine());
+
+            if ((leter >= 'a') && (leter <= 'z'))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
