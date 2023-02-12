a46c9849349d66fd8f21315bdb9bb2564b714973
Slav402 <s.zekow@gmail.com>
2023-02-13 00:35:07 +0200

    Add tasks from DefiningClasses

---
+++ C#Advanced/08.DefinigClasses1/02.CarExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            car.Make = "Peugeot";
+            car.Model = "307";
+            car.Year = 2005;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 2;
+            car.Drive(10);
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
