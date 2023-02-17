64ec6bd12ea249658a0960f35d4d8c944f8117b3
Slav402 <s.zekow@gmail.com>
2023-02-17 23:58:39 +0200

    upload task 01 from Defining Classes - Exercise

---
+++ C#Advanced/01. DefineАClassPerson/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._DefineАClassPerson
+{
+    class Person
+    {
+    }
+}
---
+++ C#Advanced/01. DefineАClassPerson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._DefineАClassPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
