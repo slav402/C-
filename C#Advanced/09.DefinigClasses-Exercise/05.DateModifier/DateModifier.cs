f8082274f5af36583727973b0d2ed63edd7f68e1
Slav402 <s.zekow@gmail.com>
2023-02-19 02:44:20 +0200

    add task 05.DateModifier ftom theme DefiningClasses

---
+++ C#Advanced/09.DefinigClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public static int GetDifBtwDatesInDays (string firstDate, string secondDate)
+        {
+            DateTime dateOne = DateTime.Parse(firstDate);
+            DateTime dateTwo = DateTime.Parse(secondDate);
+
+            TimeSpan difference = dateTwo - dateOne;
+
+            return Math.Abs(difference.Days);
+        }
+    }
+}
---
+++ C#Advanced/09.DefinigClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            int days = DateModifier.GetDifBtwDatesInDays(dateOne, dateTwo);
+
+            Console.WriteLine(days);
+        }
+    }
+}
