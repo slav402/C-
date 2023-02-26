d015cfc4e63d36a015a02fd6c1e0ac85b22c66d0
Slav402 <s.zekow@gmail.com>
2023-02-26 23:33:18 +0200

    first commit of tasks for OOP

---
+++ C#OOP/01.Inheritance/Inheritance-Lab/01.SingleInheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog bobi = new Dog();
+
+            bobi.Eat();
+            bobi.Bark();
+        }
+    }
+}
---
+++ C#OOP/01.Inheritance/Inheritance-Lab/03.HierarchicalInheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Farm 
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog bobi = new Dog();
+            bobi.Eat();
+            bobi.Bark();
+
+            Cat mona = new Cat();
+            mona.Eat();
+            mona.Meow();
+        }
+    }
+}
