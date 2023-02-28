986113947d22632dedf997533ebbaad7379c7d1c
Slav402 <s.zekow@gmail.com>
2023-02-28 23:22:23 +0200

    adding almost self made task 04.NeedForSpeed from Inheritance-Exercise

---
+++ C#OOP/01.Inheritance/Inheritance-Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double fuel = double.Parse(Console.ReadLine());
+            int hPower = int.Parse(Console.ReadLine());
+            
+            Motorcycle honda = new Motorcycle(hPower, fuel);
+            honda.Drive(3);
+
+            RaceMotorcycle yamaha = new RaceMotorcycle(hPower, fuel);
+            yamaha.Drive(3);
+
+            SportCar bmv = new SportCar(hPower, fuel);
+            bmv.Drive(3);
+
+            Console.WriteLine(honda.Fuel);
+            Console.WriteLine(yamaha.Fuel);
+            Console.WriteLine(bmv.Fuel);
+        }
+    }
+}
