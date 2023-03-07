5f29fb7dab6ae1b557efa44db5915c6dd1e3da1a
Slav402 <s.zekow@gmail.com>
2023-03-07 23:48:09 +0200

    something

--- C#OOP/01.Inheritance/Inheritance-Exercise/PlayersAndMonsters/StartUp.cs
+++ C#OOP/01.Inheritance/Inheritance-Exercise/PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,42 @@
-﻿namespace PlayersAndMonsters
-{
-    public class StartUp
-    {
-        public static void Main(string[] args)
-        {
+﻿using NUnit.Framework;
+using System;
+using System.Reflection;
+using System.Linq;
+using PlayersAndMonsters;
 
+[TestFixture]
+public class Tests_000_001
+{
+    private static Assembly ProjectAssembly = typeof(StartUp).Assembly;
+
+    [Test]
+    public void ValidateTypesExist()
+    {
+        var typesToAssert = new[]
+        {
+            "BladeKnight",
+            "DarkKnight",
+            "DarkWizard",
+            "Elf",
+            "Hero",
+            "Knight",
+            "MuseElf",
+            "SoulMaster",
+            "Wizard"
+        };
+
+        foreach (string typeName in typesToAssert)
+        {
+            Assert.That(GetType(typeName), Is.Not.Null, $"{typeName} type doesn't exist!");
         }
+    }
+
+    private static Type GetType(string name)
+    {
+        var type = ProjectAssembly
+            .GetTypes()
+            .FirstOrDefault(t => t.Name == name);
+
+        return type;
     }
 }
