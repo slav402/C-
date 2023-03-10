7d984cf4551910402e52f46b9a74fa3dcb31baa8
Slav402 <s.zekow@gmail.com>
2023-03-11 01:36:01 +0200

    loading task 05.Resturant from Inheritance-Exercise

---
+++ C#OOP/01.Inheritance/Inheritance-Exercise/Restaurant/Cake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double DefaultGrams = 250;
+        private const decimal DefaultCakePrice = 5.0M;
+        private const double DefaultCalories = 1000;
+
+        public Cake(string name) 
+            : base(name, DefaultCakePrice, DefaultGrams, DefaultCalories)
+        {
+        }
+    }
+}
