5e63db75311398baa9f242ca52a4b96da2bc5624
Slav402 <s.zekow@gmail.com>
2023-03-15 23:45:15 +0200

    Adding the first task 01.ClassBoxData from Encapsulation-Exercise

---
+++ C#OOP/02.Encapsulation/Encapsulation-Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxData
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get { return lenght; }
+            set 
+            {
+                ThrowIfInvalidSide(value, nameof(this.Length));
+                lenght = value; 
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set 
+            {
+                ThrowIfInvalidSide(value, nameof(this.Width));
+                width = value; 
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set 
+            {
+                ThrowIfInvalidSide(value, nameof(this.Length));
+                height = value; 
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * (Length * Width + Length * Height + Width * Height);
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * (Length + Width) * Height;
+        }
+
+        public double CalculateVolume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+
+        private void ThrowIfInvalidSide(double value, string sideName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{sideName} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ C#OOP/02.Encapsulation/Encapsulation-Exercise/01.ClassBoxData/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BoxData
+{
+    public class StratUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double heigth = double.Parse(Console.ReadLine());
+
+                Box someBox = new Box(length, width, heigth);
+
+                Console.WriteLine($"Surface Area - {someBox.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {someBox.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {someBox.CalculateVolume():f2}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
