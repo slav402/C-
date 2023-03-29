44d6dab689cf79bc3b8b041a77478e6b9bdfd988
Slav402 <s.zekow@gmail.com>
2023-03-29 23:53:03 +0300

    Upload first step for task 07.MilitaryElite

---
+++ C#OOP/03.InterfacesAndAbstraction/InterfaceAndAbstraction-Exercise/07.MilitaryElite/Models/Interfaces/ISoldier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite.Models.Interfaces
+{
+    public interface ISoldier
+    {
+        string Id { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}
---
+++ C#OOP/03.InterfacesAndAbstraction/InterfaceAndAbstraction-Exercise/07.MilitaryElite/Models/Soldier.cs
@@ -0,0 +1,13 @@
+﻿using MilitaryElite.Models.Interfaces;
+
+namespace MilitaryElite.Models
+{
+    public class Soldier : ISoldier
+    {
+        public string Id => throw new System.NotImplementedException();
+
+        public string FirstName => throw new System.NotImplementedException();
+
+        public string LastName => throw new System.NotImplementedException();
+    }
+}
