98110d65ba618c3485a4ddc9665851e62cb30b01
Slav402 <s.zekow@gmail.com>
2023-04-07 23:55:01 +0300

    Add fix for task 07.MilitaryElite

--- C#OOP/03.InterfacesAndAbstraction/InterfaceAndAbstraction-Exercise/07.MilitaryElite/Models/Commando.cs
+++ C#OOP/03.InterfacesAndAbstraction/InterfaceAndAbstraction-Exercise/07.MilitaryElite/Models/Commando.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using MilitaryElite.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MilitaryElite.Models
 {
-    public class Commando : SpecialisedSoldier
+    public class Commando : SpecialisedSoldier, ICommando
     {
-        public Commando(int id, string firstName, string lastName, decimal salary) : base(id, firstName, lastName, salary)
+        public Commando(int id, string firstName, string lastName, decimal salary) : base(id,
+                                                                                          firstName,
+                                                                                          lastName,
+                                                                                          salary)
         {
         }
+
+        public IReadOnlyCollection<IMission> Missions => throw new NotImplementedException();
     }
 }
--- C#OOP/03.InterfacesAndAbstraction/InterfaceAndAbstraction-Exercise/07.MilitaryElite/Models/Engineer.cs
+++ C#OOP/03.InterfacesAndAbstraction/InterfaceAndAbstraction-Exercise/07.MilitaryElite/Models/Engineer.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿using MilitaryElite.Models.Enums;
+using MilitaryElite.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MilitaryElite.Models
 {
-    public class Engineer : SpecialisedSoldier
+    public class Engineer : SpecialisedSoldier, IEngineer
     {
-        public Engineer(int id, string firstName, string lastName, decimal salary) : base(id, firstName, lastName, salary)
+        public Engineer(int id, string firstName, string lastName, decimal salary, Corps corps, IReadOnlyCollection<IRepair> repairs) 
+            : base(id, firstName, lastName, salary, corps)
         {
+            Repairs = repairs;
+        }
+
+        public IReadOnlyCollection<IRepair> Repairs {get; private set;}
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString());
+            sb.AppendLine("Repairs:");
+
+            foreach (IRepair part in Repairs)
+            {
+                sb.AppendLine($"  {part.ToString()}");
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
