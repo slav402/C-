5f44157ad89af5250c2ba0dddb80c6d85626ba85
Slav402 <s.zekow@gmail.com>
2023-04-12 23:55:44 +0300

    Loading task 044.BorderControl

---
+++ C#OOP/03.InterfacesAndAbstraction/InterfaceAndAbstraction-Exercise/04.BorederControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BorederControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
