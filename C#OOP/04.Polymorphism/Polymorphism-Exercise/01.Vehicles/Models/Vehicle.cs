c1e916f3d4465b5b57aa8b360ea10632ddc26ad5
Slav402 <s.zekow@gmail.com>
2023-05-06 23:52:25 +0300

    Upgrade task 01.Vehicles

--- C#OOP/04.Polymorphism/Polymorphism-Exercise/01.Vehicles/Models/Vehicle.cs
+++ C#OOP/04.Polymorphism/Polymorphism-Exercise/01.Vehicles/Models/Vehicle.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vehicles.Models.Interfaces;
 
 namespace Vehicles.Models
 {
-    internal class Vehicle
+    public abstract class Vehicle : IVehicle
     {
+
+        private double increaseConsumption;
+        
+        protected Vehicle(double fuelQuantity, double fuelConsumption, double increaseConsumption)
+        {
+            FuelQuantity = fuelQuantity;
+            FueluelConsumption = fuelConsumption;
+            this.increaseConsumption = increaseConsumption;
+        }
+
+        public double FuelQuantity { get; private set; }
+
+        public double FueluelConsumption { get; private set; }
+
+        public string Drive(double distance)
+        {
+            double fuel
+        }
+
+        public void Refuel(double liters)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
