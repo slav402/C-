44ab0437702c45e15d1d2a108038929c359e179d
Slav402 <s.zekow@gmail.com>
2023-06-11 23:55:34 +0300

    Initial add task 03.Raiding

---
+++ C#OOP/04.Polymorphism/Polymorphism-Exercise/03.Raiding/IO/ConsoleReader.cs
@@ -0,0 +1,11 @@
+﻿using Raiding.IO.Interfaces;
+
+
+namespace Raiding.IO
+{
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+            => Console.ReadLine();
+    }
+}
---
+++ C#OOP/04.Polymorphism/Polymorphism-Exercise/03.Raiding/Models/Warrior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Models
+{
+    internal class Warrior : BaseHero
+    {
+        public Warrior(string name, int power) 
+            : base(name, power)
+        {
+        }
+
+        public override string CastAbility()
+        => $"{this.GetType().Name} - {Name} hit for {Power} damage";
+    }
+}
