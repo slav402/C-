1737c59e4da633e553fa14f701a443f4b7872416
Slav402 <s.zekow@gmail.com>
2023-06-12 23:59:07 +0300

    add update for task 03.Raiding

--- C#OOP/04.Polymorphism/Polymorphism-Exercise/03.Raiding/Engine/Engine.cs
+++ C#OOP/04.Polymorphism/Polymorphism-Exercise/03.Raiding/Engine/Engine.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Raiding.Engine.Interfaces;
+using Raiding.IO.Interfaces;
+using Raiding.Models.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Raiding.Engine
 {
-    public class Engine : Ien
+    public class Engine : IEngine
     {
+        private IReader reader;
+        private IWriter writer;
+
+        private readonly ReadOnlyCollection<IBaseHero> riderSquad;
+
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ C#OOP/04.Polymorphism/Polymorphism-Exercise/03.Raiding/Factories/HeroFactory.cs
@@ -0,0 +1,31 @@
+﻿using Raiding.Factories.Interfaces;
+using Raiding.Models;
+using Raiding.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding.Factories
+{
+    public class HeroFactory : IBaseHeroFactories
+    {
+        public IBaseHero CreateHero(string type, string name)
+        {
+            switch (type)
+            {
+                case "Druid":
+                    return new Druid(name, 80);
+                case "Paladin":
+                    return new Paladin(name, 100);
+                case "Rogue":
+                    return new Rogue(name, 80);
+                case "Warrior":
+                    return new Warrior(name, 100);
+                default:
+                    throw new ArgumentException("Invalid hero!"); ;
+            }
+        }
+    }
+}
