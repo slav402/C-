921a9236f473b5c479ae8425e71ebd041610ec54
Slav402 <s.zekow@gmail.com>
2023-08-10 12:32:25 +0300

    More labs for Reflection

---
+++ C#OOP/07.Reflection/Reflection-Lab/ReadAllFields/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Reflection;
+
+Product product = new Product();
+
+PrintAllFields(product);
+PrintAllFields(DateTime.Now);
+
+
+void PrintAllFields(object obj)
+{
+    Type type = obj.GetType();
+    FieldInfo[] fields = type.GetFields();
+
+    foreach (FieldInfo field in fields)
+    {
+        Console.WriteLine($"Name: {field.Name}");
+        Console.WriteLine($"DeclaringType: {field.DeclaringType}");
+        Console.WriteLine($"IsFamily: {field.IsFamily}");
+        Console.WriteLine($"IsPublic: {field.IsPublic}");
+        Console.WriteLine($"IsStatic: {field.IsStatic}");
+        Console.WriteLine($"{field.Name} - {field.GetValue(obj)}");
+        Console.WriteLine();
+    }
+}
+
+public class Product
+{
+    public static int id;
+    public decimal price;
+    public string Model;
+}
---
+++ C#OOP/07.Reflection/Reflection-Lab/WichPropYouWantToRead/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+while (true)
+{
+    Console.WriteLine("Wich property you want to read?");
+    string propName = Console.ReadLine();
+
+	try
+	{
+		GetPropertyValue(propName, DateTime.Now);
+	}
+	catch (Exception)
+	{
+        Console.WriteLine("No such property!");
+    }
+
+}
+
+void GetPropertyValue(string propName, object obj)
+{
+	Type type = obj.GetType();
+
+	PropertyInfo propInfo = type.GetProperty(propName);
+
+    Console.WriteLine(propInfo.GetValue(obj));
+}
