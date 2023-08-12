67e96cbb300817b1439524a359cbb29934003be9
Slav402 <s.zekow@gmail.com>
2023-08-13 00:16:04 +0300

    Unit testing

--- C#OOP/08.UnitTesting/UnitTesting-Lab-Skeleton/Skeleton.Tests/AxeTests.cs
+++ C#OOP/08.UnitTesting/UnitTesting-Lab-Skeleton/Skeleton.Tests/AxeTests.cs
@@ -1,4 +1,7 @@
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +9,38 @@ namespace Skeleton.Tests
     public class AxeTests
     {
         [Test]
-        public void Test1()
+        public void AXEGettersTest()
         {
+            Axe axe = new Axe(10, 10);
 
+            Assert.AreEqual(10, axe.AttackPoints);
+            Assert.AreEqual(10, axe.DurabilityPoints);
+        }
+
+        [Test]
+        public void AxeAttakShuldDropDurability()
+        {
+            //Arrange
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            //Act
+            axe.Attack(dummy);
+
+            //Asset
+            Assert.AreEqual(9, axe.DurabilityPoints, "Axe durability doesn't change after attack.");
+        }
+
+        [Test]
+        public void TestAttackingWithBrokenWeapon()
+        {
+            //Arrange
+            Axe axe = new Axe(10, 0);
+            Dummy dummy = new Dummy(10, 10);
+
+            //Act
+            //Asset
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
         }
     }
 }
--- C#OOP/08.UnitTesting/UnitTesting-Lab-Skeleton/Skeleton.Tests/DummyTests.cs
+++ C#OOP/08.UnitTesting/UnitTesting-Lab-Skeleton/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,51 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void DummyLosesHealthIfAttackedTest()
         {
+            //Arrange
+            Dummy dummy = new Dummy(10, 10);
 
+            //Act
+            dummy.TakeAttack(5);
+
+            //Asset
+            Assert.AreEqual(5, dummy.Health);
+        }
+
+        [Test]
+        public void DeadDummyThrowsAnExceptionIfAttackedTest()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(0, 10);
+
+            //Act
+            //Asset
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(5));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXPTest()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(0, 10);
+
+            //Act
+            int exp = dummy.GiveExperience();
+
+            //Asset
+            Assert.AreEqual(10, exp);
+        }
+
+        [Test]
+        public void AliveDummyCanNotGiveXPTest()
+        {
+            //Arrange
+            Dummy dummy = new Dummy(10, 10);
+
+            //Act
+            //Asset
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
         }
     }
 }
